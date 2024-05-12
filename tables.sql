b1d09a322dc5b2dcc5ab2c99812c1b7e5295473c
Razie <rzyh1381@gmail.com>
2024-05-12 23:40:34 +0330

    update sql for table creation

--- tables.sql
+++ tables.sql
@@ -1,186 +1,207 @@
-create database divar;
+create database Divar;
 show databases;
 
 # entity
-CREATE TABLE divar.normal_user(
-    userID int primary key not null,
-    first_name varchar(255),
-    last_name varchar(255),
-	registered_at datetime not null,
-    country varchar(255),
-    state varchar(255),
-    city varchar(255),
-    street varchar(255),
-    house_num int,
-    is_active boolean
+CREATE TABLE divar.Creator(
+    CreatorID int NOT NULL, 
+    IsActive boolean,
+    PRIMARY KEY(CreatorID)
 );
 
 # entity
-CREATE TABLE divar.admin_user(
-    userID int primary key not null references user,
-    first_name varchar(255),
-    last_name varchar(255),
-	registered_at datetime not null,
-    country varchar(255),
-    state varchar(255),
-    city varchar(255),
-    street varchar(255),
-    house_num int
+CREATE TABLE divar.NormalUser(
+    UserID int NOT NULL,
+    CreatorID int NOT NULL,
+    FirstName varchar(255),
+    LastName varchar(255),
+	RegisteredAt DATETIME NOT NULL,
+    Email varchar(255) NOT NULL,
+    Phone varchar(255) NOT NULL,
+    Country varchar(255),
+    State varchar(255),
+    City varchar(255) NOT NULL,
+    Street varchar(255),
+    House_num int,
+    PRIMARY KEY (UserID),
+    FOREIGN KEY(CreatorID) REFERENCES Creator(CreatorID)
 );
 
-# error
+# entity
+CREATE TABLE divar.AdminUser(
+    UserID int NOT NULL,
+    FirstName varchar(255),
+    LastName varchar(255),
+	RegisteredAt DATETIME NOT NULL,
+    Email varchar(255) NOT NULL,
+    Phone varchar(255) NOT NULL,
+    Country varchar(255),
+    State varchar(255),
+    City varchar(255) NOT NULL,
+    Street varchar(255),
+    House_num int,
+    PRIMARY KEY (UserID),
+    FOREIGN KEY(UserID) REFERENCES NormalUser(UserID)
+);
+
+# entity
+CREATE TABLE divar.Business(
+    BusinessID int NOT NULL,
+    UserID int NOT NULL,
+    CreatorID int NOT NULL,
+    BusName varchar(255) NOT NULL,
+    Category varchar(255),
+    RegistrationNum int NOT NULL,
+    Country varchar(255),
+    State varchar(255),
+    City varchar(255) NOT NULL,
+    Street varchar(255),
+    HouseNum int,
+    PRIMARY KEY(UserID, BusinessID),
+    FOREIGN KEY(UserID) REFERENCES NormalUser(UserID),
+    FOREIGN KEY(CreatorID) REFERENCES Creator(CreatorID)
+);
+
+# entity
+CREATE TABLE divar.Advertise(
+    AdID int NOT NULL,
+    CreatorID int NOT NULL,
+    Title varchar(255) not null,
+    Price varchar(255) not null,
+    Descriptions varchar(255),
+    Subtitle varchar(255),
+    Country varchar(255),
+    State varchar(255),
+    City varchar(255) NOT NULL,
+    Street varchar(255),
+    HouseNum int,
+    CreatedAt DATETIME NOT NULL,
+    UpdatedAt DATETIME NOT NULL,
+    PRIMARY KEY(CreatorID, AdID),
+    FOREIGN KEY(CreatorID) REFERENCES Creator(CreatorID)
+);
+
 # MN relation
-CREATE TABLE divar.visit(
-    userID int primary key not null references normal_user,
-    adID int primary key not null references advertise
+CREATE TABLE divar.Visit(
+    UserID int NOT NULL,
+    CreatorID int NOT NULL,
+    AdID int NOT NULL,
+    PRIMARY KEY(UserID, CreatorID, AdID),
+    FOREIGN KEY(UserID) REFERENCES NormalUser(UserID),
+    FOREIGN KEY(CreatorID, AdID) REFERENCES Advertise(CreatorID, AdID)
 );
 
 # multivalue adj
-CREATE TABLE divar.email(
-    userID int not null references normal_user,
-    email varchar(255) primary key not null
+CREATE TABLE divar.Images(
+	AdID int NOT NULL,
+    CreatorID int NOT NULL,
+    ImagePath varchar(255) NOT NULL,
+    PRIMARY KEY(ImagePath,CreatorID, AdID),
+    FOREIGN KEY(CreatorID, AdID) REFERENCES Advertise(CreatorID, AdID)
 );
 
-# multivalue adj
-CREATE TABLE divar.phone(
-    userID int not null references normal_user,
-    phone varchar(255) primary key not null
-);
-
-# entity
-CREATE TABLE divar.business(
-    businessID int primary key not null,
-    userID int not null references normal_user,
-    bus_name varchar(255) not null,
-    category varchar(255),
-    registration_num int,
-    country varchar(255),
-    state varchar(255),
-    city varchar(255),
-    street varchar(255),
-    house_num int
-);
-
-# entity
-CREATE TABLE divar.creator(
-    creatorID int primary key not null,
-    creator_type varchar(255) not null, 
-    is_active boolean
-);
-
-# error 
-# entity
-CREATE TABLE divar.advertise(
-    adID int primary key not null,
-    creatorID int primary key not null references normal_user,
-    title varchar(255) not null,
-    price varchar(255) not null,
-    descriptions varchar(255),
-    subtitle varchar(255),
-    country varchar(255),
-    state varchar(255),
-    city varchar(255),
-    street varchar(255),
-    house_num int,
-    created_at datetime not null,
-    updated_at datetime not null
-);
-
-# can't test because of ad
-# multivalue adj
-CREATE TABLE divar.images(
-    adID int not null references advertise,
-    image blob primary key not null
-);
-
-# error
 # weak entity
-# how to have sth like enum (specific values)
-# isn't it better to add this to ad table?
-CREATE TABLE divar.ad_status(
-	adID int primary key not null references advertise,
-    status_comment varchar(255),
-    state ??? not null,
-    updated_at datetime not null
+CREATE TABLE divar.AdStatus(
+	AdID int NOT NULL,
+    CreatorID int NOT NULL,
+    StatusComment varchar(255),
+    State ENUM('DELETED','CONFIRMED','REJECTED', 'NONE') NOT NULL,
+    UpdatedAt DATETIME NOT NULL,
+    PRIMARY KEY(CreatorID, AdID),
+    FOREIGN KEY(CreatorID, AdID) REFERENCES Advertise(CreatorID, AdID)
 );
 
-# error
 # MN relation
-CREATE TABLE divar.modify_status(
-	adID int primary key not null references advertise,
-    userID int primary key not null references admin_user
+CREATE TABLE divar.ModifyStatus(
+	UserID int NOT NULL,
+    CreatorID int NOT NULL,
+    AdID int NOT NULL,
+    PRIMARY KEY(UserID, CreatorID, AdID),
+    FOREIGN KEY(UserID) REFERENCES AdminUser(UserID),
+    FOREIGN KEY(CreatorID, AdID) REFERENCES Advertise(CreatorID, AdID)
 );
 
-# error
 # weak entity
-# sefat momayeze (Discriminator) ???
-CREATE TABLE divar.ad_report(
-	report_num int primary key not null,
-    adID int primary key not null references advertise,
-    userID int not null references normal_user,
-    category varchar(255) not null,
-    content varchar(255)
+CREATE TABLE divar.AdReport(
+	ReportNum int NOT NULL,
+    CreatorID int NOT NULL,
+    AdID int NOT NULL,
+    UserID int NOT NULL,
+    Category varchar(255) NOT NULL,
+    Content varchar(255),
+    PRIMARY KEY(ReportNum, CreatorID, AdID),
+    FOREIGN KEY(CreatorID, AdID) REFERENCES Advertise(CreatorID, AdID),
+    FOREIGN KEY(UserID) REFERENCES NormalUser(UserID)
 );
 
-# error
 # MN relation
-CREATE TABLE divar.check_report(
-	adID int primary key not null references report,
-    report_num int primary key not null references report,
-    userID int primary key not null references normal_user
+CREATE TABLE divar.CheckReport(
+	ReportNum int NOT NULL,
+    CreatorID int NOT NULL,
+    AdID int NOT NULL,
+    UserID int NOT NULL,
+	PRIMARY KEY(ReportNum, CreatorID, AdID, UserID),
+    FOREIGN KEY(ReportNum) REFERENCES AdReport(ReportNum),
+    FOREIGN KEY(CreatorID, AdID) REFERENCES Advertise(CreatorID, AdID),
+    FOREIGN KEY(UserID) REFERENCES AdminUser(UserID)
 );
 
-# can't test because of ad
 # entity
-CREATE TABLE divar.general_cat(
-    adID int primary key not null references advertise
+CREATE TABLE divar.GeneralCat(
+	AdID int NOT NULL,
+    CreatorID int NOT NULL,
+    PRIMARY KEY(CreatorID, AdID),
+    FOREIGN KEY(CreatorID, AdID) REFERENCES Advertise(CreatorID, AdID)
 );
 
-# can't test because of ad
-# error
 # entity
-CREATE TABLE divar.transportation_cat(
-    adID int primary key not null references advertise,
-    creatorID int primary key not null references normal_user,
-    brand varchar(255),
-    vehicle_type varchar(255),
-    vehicle_engine varchar(255)
+CREATE TABLE divar.TransportationCat(
+	AdID int NOT NULL,
+    CreatorID int NOT NULL,
+    Brand varchar(255),
+    VehicleType varchar(255),
+    VehicleEngine varchar(255),
+    PRIMARY KEY(CreatorID, AdID),
+    FOREIGN KEY(CreatorID, AdID) REFERENCES Advertise(CreatorID, AdID)
 );
 
-# can't test because of ad
-# error
 # entity
-CREATE TABLE divar.home_cat(
-    adID int primary key not null references advertise,
-    creatorID int primary key not null references normal_user,
-    factory varchar(255),
-    size varchar(255),
-    material varchar(255)
+CREATE TABLE divar.HomeCat(
+	AdID int NOT NULL,
+    CreatorID int NOT NULL,
+    Factory varchar(255),
+    Size varchar(255),
+    Material varchar(255),
+    PRIMARY KEY(CreatorID, AdID),
+    FOREIGN KEY(CreatorID, AdID) REFERENCES Advertise(CreatorID, AdID)
 );
 
-# can't test because of ad
-# error
 # entity
-CREATE TABLE divar.real_cat(
-    adID int primary key not null references advertise,
-    creatorID int primary key not null references normal_user,
-    meterage varchar(255),
-    title_deed varchar(255),
-    constructed_at datetime not null
+CREATE TABLE divar.RealCat(
+	AdID int NOT NULL,
+    CreatorID int NOT NULL,
+    Meterage varchar(255),
+    TitleDeed varchar(255),
+    ConstructedAt DATETIME NOT NULL,
+    PRIMARY KEY(CreatorID, AdID),
+    FOREIGN KEY(CreatorID, AdID) REFERENCES Advertise(CreatorID, AdID)
 );
 
-# can't test because of ad
-# error
 # entity
-CREATE TABLE divar.transportation_cat(
-    adID int primary key not null references advertise,
-    creatorID int primary key not null references normal_user,
-    brand varchar(255),
-    model varchar(255),
-    battery varchar(255)
+CREATE TABLE divar.DigitalCat(
+	AdID int NOT NULL,
+    CreatorID int NOT NULL,
+    Brand varchar(255),
+    Model varchar(255),
+    Battery varchar(255),
+    PRIMARY KEY(CreatorID, AdID),
+    FOREIGN KEY(CreatorID, AdID) REFERENCES Advertise(CreatorID, AdID)
 );
 
-drop table admin_user;
-desc divar.email; 
-show tables from divar;
-drop database divar;
+desc divar.NormalUser; 
+desc divar.AdminUser; 
+desc divar.Business; 
+desc divar.Creator;
+desc divar.Advertise;
+show tables from Divar;
+drop table Divar.AdminUser;
+drop database Divar;
